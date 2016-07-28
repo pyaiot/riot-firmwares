6231be8ca462fd4afc8dca6b80112bffa06fdbe0
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-07-28 09:35:02 +0200

    exposing led on iotlab a8 m3 node

--- firmwares/node_a8_m3/coap.c
+++ firmwares/node_a8_m3/coap.c
@@ -33,11 +33,24 @@ static int handle_get_temperature(coap_rw_buffer_t *scratch,
                                   coap_packet_t *outpkt,
                                   uint8_t id_hi, uint8_t id_lo);
 
+static int handle_get_led(coap_rw_buffer_t *scratch,
+                          const coap_packet_t *inpkt,
+                          coap_packet_t *outpkt,
+                          uint8_t id_hi, uint8_t id_lo);
+
+static int handle_put_led(coap_rw_buffer_t *scratch,
+                          const coap_packet_t *inpkt,
+                          coap_packet_t *outpkt,
+                          uint8_t id_hi, uint8_t id_lo);
+
 static const coap_endpoint_path_t path_well_known_core =
         { 2, { ".well-known", "core" } };
 
 static const coap_endpoint_path_t path_temperature =
         { 1, { "temperature" } };
+
+static const coap_endpoint_path_t path_led =
+        { 1, { "led" } };
 
 const coap_endpoint_t endpoints[] =
 {
@@ -45,6 +58,10 @@ const coap_endpoint_t endpoints[] =
       &path_well_known_core, "ct=40" },
     { COAP_METHOD_GET,	handle_get_temperature,
       &path_temperature,   "ct=0"  },
+    { COAP_METHOD_GET,	handle_get_led,
+      &path_led,	"ct=0"  },
+    { COAP_METHOD_PUT,	handle_put_led,
+      &path_led,	"ct=0"  },
     /* marks the end of the endpoints array: */
     { (coap_method_t)0, NULL, NULL, NULL }
 };
@@ -168,4 +185,43 @@ static int handle_get_temperature(coap_rw_buffer_t *scratch,
     return coap_make_response(scratch, outpkt, (const uint8_t *)response, strlen(temperature),
                               id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CONTENT,
                               COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_get_led(coap_rw_buffer_t *scratch,
+                          const coap_packet_t *inpkt,
+                          coap_packet_t *outpkt,
+                          uint8_t id_hi, uint8_t id_lo)
+{
+    char led_status[1];
+    sprintf(led_status, "%d", 1 - gpio_read(LED0_PIN));
+    memcpy(response, led_status, 1);
+
+    return coap_make_response(scratch, outpkt, (const uint8_t *)response, 1,
+                              id_hi, id_lo, &inpkt->tok, COAP_RSPCODE_CONTENT,
+                              COAP_CONTENTTYPE_TEXT_PLAIN);
+}
+
+static int handle_put_led(coap_rw_buffer_t *scratch,
+                          const coap_packet_t *inpkt,
+                          coap_packet_t *outpkt,
+                          uint8_t id_hi, uint8_t id_lo)
+{
+  coap_responsecode_t resp = COAP_RSPCODE_CHANGED;
+
+  /* On vérifie que la valeur donnée est correcte (0 ou 1)*/
+  uint8_t val = inpkt->payload.p[0];
+  if ((inpkt->payload.len == 1) &&
+    ((val == '1') || (val == '0'))) {
+    /* écriture de la nouvelle valeur de la led */
+    gpio_write(LED0_PIN, (val - '1'));
+  }
+  else {
+    resp = COAP_RSPCODE_BAD_REQUEST;
+  }
+
+  /* Réponse faite au client */
+  return coap_make_response(scratch, outpkt, NULL, 0,
+                            id_hi, id_lo,
+                            &inpkt->tok, resp,
+                            COAP_CONTENTTYPE_TEXT_PLAIN);
 }
