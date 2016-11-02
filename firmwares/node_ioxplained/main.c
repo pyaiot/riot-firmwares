b200c322afb4cdc96afbeeb8e12b8447a3c30977
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-11-02 10:33:44 +0100

    make it possible to configure channel and broker ip and port at compile time

--- firmwares/node_ioxplained/main.c
+++ firmwares/node_ioxplained/main.c
@@ -28,6 +28,14 @@
 #include "net/gnrc/ipv6.h"
 #include "net/conn/udp.h"
 
+#ifndef BROKER_ADDR
+#define BROKER_ADDR "2001:660:3207:102::4"
+#endif
+
+#ifndef BROKER_PORT
+#define BROKER_PORT 8888
+#endif
+
 #define INTERVAL              (30000000U)    /* set interval to 30 seconds */
 #define MAIN_QUEUE_SIZE       (8)
 #define BEACONING_QUEUE_SIZE  (8)
@@ -41,40 +49,43 @@ void microcoap_server_loop(void);
 extern int _netif_config(int argc, char **argv);
 
 /* broker  */
-static const ipv6_addr_t broker_addr = {{ 0xfd, 0x00, 0xab, 0xad, \
-                                          0x00, 0x1e, 0x01, 0x02, \
-                                          0x00, 0x00, 0x00, 0x00, \
-                                          0x00, 0x00, 0x00, 0x01 }};
-static uint16_t BROKER_PORT = 8888;
+static const char * broker_addr = BROKER_ADDR;
+static uint16_t broker_port = BROKER_PORT;
 
 static void *beaconing_thread(void *args)
 {
     msg_init_queue(_beaconing_msg_queue, BEACONING_QUEUE_SIZE);
-    
-    uint32_t last_wakeup = xtimer_now();
-    char message[5] = "Alive";
-    
-    for(;;) {
-	/* send data to server */
-	if (conn_udp_sendto(message, strlen(message), NULL, 0, (struct sockaddr *)&broker_addr,
-			    sizeof(broker_addr), AF_INET6, (uint16_t)0, BROKER_PORT) < 0) {
-	    printf("Error: couldn't send message to broker.\n");
-	}
 
-	/* wait 5 seconds */
-	xtimer_usleep_until(&last_wakeup, INTERVAL);
+    char message[5] = "Alive";
+
+    /* format destination address from string */
+    ipv6_addr_t dst_addr;
+    if (ipv6_addr_from_str(&dst_addr, broker_addr) == NULL) {
+        printf("Error: address not valid '%s'\n", broker_addr);
+        return NULL;
+    }
+
+    for(;;) {
+    /* send data to server */
+    if (conn_udp_sendto(message, strlen(message), NULL, 0, (struct sockaddr *)&dst_addr,
+                sizeof(dst_addr), AF_INET6, (uint16_t)0, broker_port) < 0) {
+        printf("Error: couldn't send message to broker.\n");
+    }
+
+    /* wait 5 seconds */
+    xtimer_usleep(INTERVAL);
     }
 
     return NULL;
 }
 
 int main(void)
-{   
+{
     puts("RIOT microcoap example application");
 
     /* microcoap_server uses conn which uses gnrc which needs a msg queue */
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
-    
+
     puts("Waiting for address autoconfiguration...");
     xtimer_sleep(3);
 
@@ -82,19 +93,19 @@ int main(void)
     puts("Configured network interfaces:");
     _netif_config(0, NULL);
 
-    /* create the beaconning thread that will send periodic messages to 
+    /* create the beaconning thread that will send periodic messages to
        the broker */
     int beacon_pid = thread_create(beaconing_stack, sizeof(beaconing_stack),
                                    THREAD_PRIORITY_MAIN - 1,
-				   THREAD_CREATE_STACKTEST, beaconing_thread,
+                   THREAD_CREATE_STACKTEST, beaconing_thread,
                                    NULL, "Beaconing thread");
     if (beacon_pid == -EINVAL || beacon_pid == -EOVERFLOW) {
-	puts("Error: failed to create beaconing thread, exiting\n");
+    puts("Error: failed to create beaconing thread, exiting\n");
     }
     else {
-	puts("Successfuly created beaconing thread !\n");
+    puts("Successfuly created beaconing thread !\n");
     }
-    
+
     /* start coap server loop */
     microcoap_server_loop();
 
