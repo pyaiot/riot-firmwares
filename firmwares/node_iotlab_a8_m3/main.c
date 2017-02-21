5f40ec4016a174be7158c00ad1e4663df02de5a2
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-02-21 17:09:25 +0100

    update license version to LGPLv3

--- firmwares/node_iotlab_a8_m3/main.c
+++ firmwares/node_iotlab_a8_m3/main.c
@@ -2,7 +2,7 @@
  * Copyright (C) 2017 Inria
  *
  * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
+ * General Public License v3. See the file LICENSE in the top level
  * directory for more details.
  */
 
