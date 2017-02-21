5b0c38f040c4eb1825197cf8d1378bc8daadfed3
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-02-21 17:06:30 +0100

    update copyright

--- firmwares/node_tsl2561/coap.c
+++ firmwares/node_tsl2561/coap.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 Kaspar Schleiser <kaspar@schleiser.de>
+ * Copyright (C) 2017 Inria
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
