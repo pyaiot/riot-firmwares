b3364da40028fe5a3e7acb0fa15faebbc15c6f62
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-07-04 16:55:30 +0200

    add demo node for jdev

---
+++ apps/node_jdev/main.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2017 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include "nanocoap.h"
+#include "net/gcoap.h"
+
+/* RIOT firmware libraries */
+#include "coap_common.h"
+#include "coap_led.h"
+#include "coap_position.h"
+#include "coap_bme280.h"
+
+#define MAIN_QUEUE_SIZE       (8)
+static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
+
+/* import "ifconfig" shell command, used for printing addresses */
+extern int _netif_config(int argc, char **argv);
+
+/* CoAP resources (alphabetical order) */
+static const coap_resource_t _resources[] = {
+    { "/board", COAP_GET, board_handler },
+    { "/humidity", COAP_GET, bme280_humidity_handler },
+    { "/led", COAP_GET | COAP_POST | COAP_PUT, led_handler },
+    { "/mcu", COAP_GET, mcu_handler },
+    { "/name", COAP_GET, name_handler },
+    { "/os", COAP_GET, os_handler },
+    { "/position", COAP_GET, position_handler },
+    { "/pressure", COAP_GET, bme280_pressure_handler },
+    { "/temperature", COAP_GET, bme280_temperature_handler },
+};
+
+static gcoap_listener_t _listener = {
+    (coap_resource_t *)&_resources[0],
+    sizeof(_resources) / sizeof(_resources[0]),
+    NULL
+};
+
+int main(void)
+{
+    puts("RIOT JDev Node application");
+
+    /* gnrc which needs a msg queue */
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
+
+    puts("Waiting for address autoconfiguration...");
+    xtimer_sleep(3);
+
+    /* print network addresses */
+    puts("Configured network interfaces:");
+    _netif_config(0, NULL);
+
+    /* start coap server loop */
+    gcoap_register_listener(&_listener);
+    init_beacon_sender();
+    init_bme280_sender(true, true, true);
+
+    return 0;
+}
