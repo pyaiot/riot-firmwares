f521b06ee7be8d12f58088604f49122372234066
Alexandre Abadie <alexandre.abadie@inria.fr>
2017-07-04 16:55:04 +0200

    module/coap_position: improve position customization

--- modules/coap_position/coap_position.c
+++ modules/coap_position/coap_position.c
@@ -11,16 +11,23 @@
 #define ENABLE_DEBUG (1)
 #include "debug.h"
 
-#ifndef NODE_POSITION
-#define NODE_POSITION "{\"lat\":48.714784,\"lng\":2.205502}"
+#ifndef NODE_LAT
+#define NODE_LAT "48.714784"
 #endif
+#ifndef NODE_LNG
+#define NODE_LNG "2.205502"
+#endif
+
+static uint8_t response[64] = { 0 };
 
 ssize_t position_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len)
 {
+    ssize_t p = 0;
     gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
-    const char *board = NODE_POSITION;
-    size_t payload_len = strlen(NODE_POSITION);
-    memcpy(pdu->payload, board, payload_len);
+    p += sprintf((char*)response, "{\"lat\":%s,\"lng\":%s}",
+                 NODE_LAT, NODE_LNG);
+    response[p] = '\0';
+    memcpy(pdu->payload, response, p);
 
-    return gcoap_finish(pdu, payload_len, COAP_FORMAT_TEXT);
+    return gcoap_finish(pdu, p, COAP_FORMAT_TEXT);
 }
