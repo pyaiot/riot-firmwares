c4aebe48d01f94b294634404ae1266feb5efdaea
Alexandre Abadie <alexandre.abadie@inria.fr>
2019-04-03 09:05:34 +0200

    css811: add application and module to interact with gas sensor using CoAP

---
+++ modules/coap_ccs811/coap_ccs811.h
@@ -0,0 +1,22 @@
+#ifndef COAP_CCS811_H
+#define COAP_CCS811_H
+
+#include <stdbool.h>
+#include <inttypes.h>
+
+#include "net/gcoap.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ssize_t ccs811_eco2_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx);
+ssize_t ccs811_tvoc_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx);
+
+void init_ccs811_sender(bool eco2, bool tvoc);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* COAP_CCS811_H */
